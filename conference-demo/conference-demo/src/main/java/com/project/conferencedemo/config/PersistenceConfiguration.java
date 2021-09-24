11238427d559e438a7505a7e4a56f318507ab17e
Gizmosoft <Kartikey.hebbar@gmail.com>
2021-09-24 20:27:30 +0530

    all CRUD implemented

--- conference-demo/conference-demo/src/main/java/com/project/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/conference-demo/src/main/java/com/project/conferencedemo/config/PersistenceConfiguration.java
@@ -10,14 +10,14 @@ import java.sql.SQLException;
 @Configuration
 public class PersistenceConfiguration {
     // this saves the defined configurations to the Spring Context
-    @Bean
-    public DataSource dataSource() throws SQLException {
-        // this checks if a configuration with same name exists, if not it creates, if yes, it'll update
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:mysql://localhost:3306/conference_demo");
-        System.out.println("Custom datasource has been initialized and set.");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() throws SQLException {
+//        // this checks if a configuration with same name exists, if not it creates, if yes, it'll update
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:mysql://localhost:3306/conference_demo");
+//        System.out.println("Custom datasource has been initialized and set.");
+//        return builder.build();
+//    }
 }
 // Why we are doing this? : for working with something complex operations/ working with multiple types of DB
 
